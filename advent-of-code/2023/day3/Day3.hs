205535aae4aa0312575a011148152ee61afe2a6e
Ante Kegalj <akegalj@gmail.com>
2023-12-04 11:16:01 +0100

    Day3 solutions for aoc2023 finished

--- advent-of-code/2023/day3/Day3.hs
+++ advent-of-code/2023/day3/Day3.hs
@@ -1,3 +1,4 @@
+import Control.Arrow ((&&&))
 import Data.Char
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -5,27 +6,35 @@ import qualified Data.Set as S
 type Point = (Int, Int)
 
 part1 :: M.Map Point Char -> Int
-part1 xs = sum . M.keys $ M.filter (any isSymbol) numNeighbours
+part1 xs = sum $ M.elems partNumbers
   where
-    numNeighbours :: M.Map Int (S.Set Char)
-    numNeighbours = M.map (\s -> S.map (xs M.!) $ (mconcat . map (allNeighbours xs M.!) $ S.toList s) S.\\ s) $ numbers xs
+    symbols = M.keys $ M.filter isSymbol xs
+    symbolNeighbours = foldMap neighbours symbols
+    isSymAdjecent = not . S.disjoint symbolNeighbours
+    partNumbers = M.filterWithKey (\k _ -> isSymAdjecent k) $ numbers xs
     isSymbol c = not $ isDigit c || c == '.'
 
-allNeighbours :: M.Map Point Char -> M.Map Point (S.Set Point)
-allNeighbours xs = M.mapWithKey (\p _ -> S.fromList $ neighbours p) xs
+part2 :: M.Map Point Char -> Int
+part2 xs = sum $ map product gears
   where
-    neighbours (px, py) = filter (flip M.member xs) [(px + x, py + y) | x <- [-1 .. 1], y <- [-1 .. 1], x /= 0 || y /= 0]
+    stars = M.keys $ M.filter isStar xs
+    gears = filter ((== 2) . length) . map findPartNumbers $ map neighbours stars
+    isAdjecent a = not . S.disjoint a
+    findPartNumbers nh = M.elems . M.filterWithKey (\k _ -> isAdjecent nh k) $ numbers xs
+    isStar c = c == '*'
 
-numbers :: M.Map Point Char -> M.Map Int (S.Set Point)
+neighbours :: Point -> S.Set Point
+neighbours (x, y) = S.fromList [(x + dx, y + dy) | dx <- [-1 .. 1], dy <- [-1 .. 1]]
+
+numbers :: M.Map Point Char -> M.Map (S.Set Point) Int
 numbers xs = collectNums (0, 0) mempty mempty mempty
   where
-    collectNums :: Point -> String -> S.Set Point -> M.Map Int (S.Set Point) -> M.Map Int (S.Set Point)
     collectNums p@(0, py) num set res
       | M.notMember p xs = res
     collectNums p@(px, py) num set res =
       let addToRes f
             | null num = f mempty mempty res
-            | otherwise = f mempty mempty (M.insert (read $ reverse num) set res)
+            | otherwise = f mempty mempty (M.insert set (read $ reverse num) res)
        in case M.lookup p xs of
             Just c
               | isDigit c -> collectNums (px + 1, py) (c : num) (S.insert p set) res
@@ -35,4 +44,4 @@ numbers xs = collectNums (0, 0) mempty mempty mempty
 parse :: String -> M.Map Point Char
 parse xs = M.fromList [((x, y), c) | (y, l) <- zip [0 ..] $ lines xs, (x, c) <- zip [0 ..] l]
 
-main = interact $ show . part1 . parse
+main = interact $ show . (part1 &&& part2) . parse
