97d20919301d7249de18dd6127180598006ee935
Ante Kegalj <akegalj@gmail.com>
2023-12-04 09:15:01 +0100

    Day4 solutions for aoc2023

---
+++ advent-of-code/2023/Day4.hs
@@ -0,0 +1,28 @@
+#!/usr/bin/env nix-shell
+#!nix-shell --pure -i runhaskell -p "haskell.packages.ghc98.ghcWithPackages (pkgs: with pkgs; [ split ])"
+
+import Control.Arrow ((&&&))
+import Data.List.Split
+import qualified Data.Set as S
+
+type Card = (S.Set Int, S.Set Int)
+
+part1 :: [Card] -> Int
+part1 = sum . map (\n -> 2 ^ (n - 1)) . filter (/= 0) . map (length . uncurry S.intersection)
+
+part2 :: [Card] -> Int
+part2 xs = copyCards (length xs) xs
+  where
+    copyCards num xs = sum $ map (\n -> go $ drop n xs) [0 .. num - 1]
+    go [] = error "Impossible"
+    go (x : xs) = isWin + copyCards winCards xs
+      where
+        winCards = length $ uncurry S.intersection x
+        isWin = if winCards >= 0 then 1 else 0
+
+parse :: String -> [Card]
+parse = map (splitNums . map (S.fromList . map read . words) . splitOn "|" . (!! 1) . splitOn ":") . lines
+  where
+    splitNums [xs, ys] = (xs, ys)
+
+main = interact $ show . (part1 &&& part2) . parse
