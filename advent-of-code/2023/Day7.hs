25fff9691b41ea54fdb8837a7d9be146cb5929f0
Ante Kegalj <akegalj@gmail.com>
2023-12-07 14:19:20 +0100

    Day7, part2

--- advent-of-code/2023/Day7.hs
+++ advent-of-code/2023/Day7.hs
@@ -3,30 +3,38 @@ import Control.Arrow ((&&&))
 import qualified Data.Map as M
 import Data.List
 
-data Card = A | K | Q | J | T | C9 | C8 | C7 | C6 | C5 | C4 | C3 | C2 deriving (Show, Eq, Ord, Enum)
-type Hand = [Card]
-data HandType = FiveKind | FourKind | FullHouse | ThreeKind | TwoPair | OnePair | HighCard deriving (Show, Eq, Ord, Enum)
-type Game = [(HandType, Hand, Int)]
+data HandType = FiveKind | FourKind | FullHouse | ThreeKind | TwoPair | OnePair | HighCard deriving (Show, Eq, Ord)
+data CamelCard = CA | CK | CQ | CJ | CT | C9 | C8 | C7 | C6 | C5 | C4 | C3 | C2 deriving (Show, Eq, Ord, Enum, Bounded)
+data JokerCamelCard = JA | JK | JQ | JT | J9 | J8 | J7 | J6 | J5 | J4 | J3 | J2 | JJ deriving (Show, Eq, Ord, Enum, Bounded)
 
-part1 :: Game -> Int
-part1 = sum . map (\(r,(_,_,b)) -> r*b) . zip [1..] . reverse . sort
-part2 = const 0
+part1 = solve . toGame @CamelCard (const False)
+part2 = solve . toGame (==JJ)
 
-parse :: String -> Game
-parse = map (parsePlayer . words) . lines
+solve :: Ord a => [(HandType, [a], Int)] -> Int
+solve = sum . map (\(r,(_,_,b)) -> r*b) . zip [1..] . reverse . sort
+
+parse :: String -> [(String, Int)]
+parse = map ((\[c, b] -> (c, read b)) . words) . lines
+
+toGame :: (Ord a, Eq a, Bounded a, Enum a, Show a) => (a -> Bool) -> [(String, Int)] -> [(HandType, [a], Int)]
+toGame isJoker = map parsePlay
   where
-    parsePlayer [cards, bid] = let hand = parseHand cards
-                               in (parseHandType hand, hand, read bid)
+    parsePlay (parseHand -> hand, bid) = (parseHandType hand, hand, bid)
     parseHand = map parseCard
-    parseCard = (M.fromList (zip "AKQJT98765432" [A .. C2]) M.!)
+    parseCard = (M.fromList (zip mapKeys cards) M.!)
+    mapKeys = map (last . show) cards
+    cards = [minBound .. maxBound]
+    swapJoker r c = if isJoker c then r else c
+    swapAllJokers hand = map (\r -> map (swapJoker r) hand) $ filter (not . isJoker) cards
+    checkCond hand f = any f . map sortByCard . swapAllJokers $ hand
     sortByCard = sortOn length . group . sort
-    parseHandType (sortByCard -> hand)
-      | length (last hand) == 5 = FiveKind
-      | length (last hand) == 4 = FourKind
-      | length (last hand) == 3 && length hand == 2 = FullHouse
-      | length (last hand) == 3 = ThreeKind
-      | length (filter ((==2) . length) hand) == 2 = TwoPair
-      | length (filter ((==2) . length) hand) == 1 = OnePair
-      | length hand == 5 = HighCard
+    parseHandType (checkCond -> check)
+      | check $ (==5) . length . last = FiveKind
+      | check $ (==4) . length . last = FourKind
+      | check $ (==(3,2)) . (length . last &&& length) = FullHouse
+      | check $ (==3) . length . last = ThreeKind
+      | check $ (==2) . length . filter ((==2) . length) = TwoPair
+      | check $ (==1) . length . filter ((==2) . length) = OnePair
+      | check $ (==5) . length = HighCard
 
 main = interact $ show . (part1 &&& part2) . parse
