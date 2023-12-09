c548799383e03f49ea20236bc224cb774757934a
Ante Kegalj <akegalj@gmail.com>
2023-12-09 11:06:08 +0100

    Day9, part2 cleanup

--- advent-of-code/2023/Day9.hs
+++ advent-of-code/2023/Day9.hs
@@ -3,13 +3,11 @@ import Data.List
 
 part1, part2 :: [[Int]] -> Int
 part1 = sum . map (foldl1' (+) . map last . extrapolate)
-part2 = sum . map (foldl1' (flip (-)) . reverse . map head . extrapolate)
+part2 = sum . map (foldr1 (-) . map head . extrapolate)
 
 extrapolate :: [Int] -> [[Int]]
-extrapolate = go
-  where
-    go [x] = [[x]]
-    go all@(_:xs) = all : go (zipWith (-) xs all)
+extrapolate [x] = [[x]]
+extrapolate all@(_:xs) = all : extrapolate (zipWith (-) xs all)
 
 parse :: String -> [[Int]]
 parse = map (map read . words) . lines
