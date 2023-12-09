952a5d54612ae3dd2006b306f7f89af816e75342
Ante Kegalj <akegalj@gmail.com>
2023-12-09 10:56:43 +0100

    Day9, part2

--- advent-of-code/2023/Day9.hs
+++ advent-of-code/2023/Day9.hs
@@ -2,11 +2,11 @@ import Control.Arrow ((&&&))
 import Data.List
 
 part1, part2 :: [[Int]] -> Int
-part1 = sum . map extrapolate
-part2 = const 0
+part1 = sum . map (foldl1' (+) . map last . extrapolate)
+part2 = sum . map (foldl1' (flip (-)) . reverse . map head . extrapolate)
 
-extrapolate :: [Int] -> Int
-extrapolate = foldl1' (+) . map last . go
+extrapolate :: [Int] -> [[Int]]
+extrapolate = go
   where
     go [x] = [[x]]
     go all@(_:xs) = all : go (zipWith (-) xs all)
