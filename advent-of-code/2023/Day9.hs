b29eabaa95458499463222fae57231bb4c9c889a
Ante Kegalj <akegalj@gmail.com>
2023-12-09 11:44:24 +0100

    Day9, part2 simplify

--- advent-of-code/2023/Day9.hs
+++ advent-of-code/2023/Day9.hs
@@ -2,12 +2,11 @@ import Control.Arrow ((&&&))
 import Data.List
 
 part1, part2 :: [[Int]] -> Int
-part1 = sum . map (foldl1' (+) . map last . extrapolate)
-part2 = sum . map (foldr1 (-) . map head . extrapolate)
+part1 = sum . map (sum . map last . extrapolate)
+part2 = part1 . map reverse
 
 extrapolate :: [Int] -> [[Int]]
-extrapolate [x] = [[x]]
-extrapolate all@(_:xs) = all : extrapolate (zipWith (-) xs all)
+extrapolate = takeWhile (any (/=0)) . iterate (zipWith subtract <*> tail)
 
 parse :: String -> [[Int]]
 parse = map (map read . words) . lines
