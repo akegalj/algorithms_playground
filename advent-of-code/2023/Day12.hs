3abd4f2d00b00c26742a735bc7720efc2fee959e
Ante Kegalj <akegalj@gmail.com>
2024-04-08 15:09:37 +0200

    aoc2023 day12 part1, optimize

--- advent-of-code/2023/Day12.hs
+++ advent-of-code/2023/Day12.hs
@@ -1,51 +1,57 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE BangPatterns #-}
 import Control.Arrow ((&&&))
-import Data.Bits
-import Data.List
 import Data.List.Split (splitOn)
-import Debug.Trace
+import Data.List (groupBy, sort, nub, foldl', intersperse)
+import qualified Data.Map as M
+import Data.Function (on)
+import Data.Bits (setBit, Bits, (.|.), shiftL, (.&.))
 
-type RecMask = Int
-type RecIx = [Int]
-type RecRes = [Int]
-type ConRec = ((RecMask, RecIx), RecRes)
-type BitNum = [Bool]
-
-fromBits :: Bits a => a -> BitNum
-fromBits = reverse . go
-  where
-    go b
-      | b == zeroBits = []
-      | otherwise     = testBit b 0 : go (shiftR b 1)
-
-groupSizes :: Bits a => a -> RecRes
-groupSizes = map length . filter (any (==True)) . group . fromBits
+data Spring = Operational | Damaged | Unknown deriving (Eq, Show)
+newtype GroupSize = GroupSize Int
+  deriving stock (Eq, Ord, Show)
+  deriving newtype Read
+type ConRec = ([Spring], [GroupSize])
 
 parseInput :: String -> [ConRec]
 parseInput = map parseLine . lines
   where
-    parseLine = (\[pattern, sx] -> ((mask pattern, indexUnknown pattern), map read $ splitOn "," sx)) . words
-    mask = foldl' setBit 0 . index '#'
-    indexUnknown = index '?'
-    index c = map fst . filter ((==c) . snd) . zip [0..] . reverse
+    parseLine = (\[l,r] -> (map parseSpring l, map read $ splitOn "," r)) . words
+    parseSpring '.' = Operational
+    parseSpring '#' = Damaged
+    parseSpring '?' = Unknown
 
-fromRecIx :: (Bits a, Num a) => RecIx -> a -> a
-fromRecIx [] 0 = 0
-fromRecIx _  0 = 0
-fromRecIx [] _ = error "boom"
-fromRecIx (i:ix) b
-  | testBit b 0 = fromRecIx ix (shiftR b 1) `setBit` i
-  | otherwise = fromRecIx ix (shiftR b 1)
+indexGroups :: ConRec -> M.Map GroupSize [Int]
+indexGroups (xs, gs) = M.fromList . map (id &&& index) . nub $ sort gs
+  where
+    toDamaged = map (\x -> if x == Unknown then Damaged else x) xs
+    contGroup = map (length &&& fst . head) . filter (any $ (==Damaged) . snd) . groupBy ((==) `on` snd) $ zip [0..] toDamaged
+    index (GroupSize g) = concatMap (\(l, i) -> [i..i+l-g]) contGroup
 
-findSizes :: (Bits a, Num a) => (a, RecIx) -> a -> (a, RecRes)
-findSizes (mask, ix) = (id &&& groupSizes) . (mask .|.) . fromRecIx ix
+springMask :: (Bits a, Num a) => [Spring] -> a
+springMask = foldl' setBit 0 . map fst . filter ((==Damaged) . snd) . zip [0..] . reverse
 
-findValidArrangements :: ConRec -> [Int]
-findValidArrangements ((mask,ix),res) = map fst . filter ((res==) . snd) $ map (findSizes (mask,ix)) [0..2^(length ix) - 1]
+indexMask :: (Bits a, Num a) => Int -> [(GroupSize, Int)] -> a
+indexMask l = foldl' (.|.) 0 . map (\(GroupSize g,i) -> shiftL (2^g-1) $ l-i-g)
+
+testMask :: Int -> ConRec -> [Int] -> Bool
+testMask sm (sx, gs) ix = sm .&. indexMask (length sx) (zip gs ix) == sm
+
+simulate :: ConRec -> [[Int]]
+simulate cr@(xs, gs) = go 0 $ zip gs groupIxs
+  where
+    ig = indexGroups cr
+    groupIxs = map (ig M.!) gs
+    go :: Int -> [(GroupSize, [Int])] -> [[Int]]
+    go _ [] = [[]]
+    go !start ((GroupSize l, is):gs) = concatMap (\i -> map (i:) $ go (i+l+1) gs) $ dropWhile (start>) is
 
 part1 :: [ConRec] -> Int
-part1 = sum . map (length . findValidArrangements)
+part1 = sum . map (\cr@(s,_) -> let sm = springMask s in length . filter (testMask sm cr) $ simulate cr)
 
 part2 :: [ConRec] -> Int
-part2 = const 0
+part2 = part1 . map expandInput
+  where
+    expandInput (sx, gs) = (concat $ intersperse [Unknown] $ replicate 5 sx, concat $ replicate 5 gs)
 
 main = interact $ show . (part1 &&& part2) . parseInput
