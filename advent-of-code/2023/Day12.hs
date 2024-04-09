ea81dc1ed43fe9e325f7d03f943cbaeb3e4c9f8f
Ante Kegalj <akegalj@gmail.com>
2024-04-09 22:52:32 +0200

    aoc2023 day12 part2, recursive

--- advent-of-code/2023/Day12.hs
+++ advent-of-code/2023/Day12.hs
@@ -6,7 +6,7 @@ import Data.List (groupBy, sort, nub, foldl', intersperse, find)
 import qualified Data.Map as M
 import Data.Function (on)
 import Data.Bits (setBit, Bits, (.|.), shiftL, (.&.), testBit)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, fromJust)
 import Control.Applicative ((<|>))
 import Debug.Trace
 
@@ -25,24 +25,28 @@ parseInput = map parseLine . lines
     parseSpring '?' = Unknown
 
 count :: GroupSize -> [Spring] -> [GroupSize] -> Maybe Int
-count 0   [] [] = Just 1
-count 0   [] [_] = Just 1
-count 0   (Damaged:_) _ = Nothing
-count 0   (Operational:ss) [_] = if Damaged `elem` ss then trace "woot" Nothing else Just 1
-count 0   (Operational:ss) (_:g:gs) = count g ss gs
-count cur (Operational:ss) ags@(g:_) = count g ss ags
-count cur (Damaged:ss) ags = count (cur-1) ss ags
-count cur (Unknown:ss) ags =
-  let c1 = count cur (Operational:ss) ags
-      c2 = count cur (Damaged:ss) ags
+count 0 [] [_] = Just 1
+count _ [] _ = Nothing
+count 0 (Operational:ss) gs = case gs of
+  _:g':gs' -> count g' ss (g':gs') -- we pick next group
+  _ -> if Damaged `elem` ss then Nothing else Just 1
+count 0 (Damaged:ss) gs = Nothing
+count 0 (Unknown:ss) gs = count 0 (Operational:ss) gs -- must be operational
+count cur (Operational:ss) ags@(g:gs)
+  | cur == g = count cur ss ags
+  | otherwise = Nothing
+count cur (Damaged:ss) gs = count (cur-1) ss gs
+count cur (Unknown:ss) gs =
+  let c1 = count cur (Operational:ss) gs
+      c2 = count cur (Damaged:ss) gs
   in ((+) <$> c1 <*> c2) <|> c1 <|> c2
-count a b _ = error $ show b
+count a b c = error $ show (a,b,c)
 
 part1 :: [ConRec] -> Int
-part1 = const 0
+part1 = sum . map (\(ss,g:gs) -> fromJust $ count g ss $ g:gs) -- FIXME
 
 part2 :: [ConRec] -> Int
-part2 = const 0 -- part1 . map expandInput
+part2 = part1 . map (\(x,y) -> trace (show x) $ expandInput y) . zip [0..]
 
 expandInput (sx, gs) = (concat $ intersperse [Unknown] $ replicate 5 sx, concat $ replicate 5 gs)
 
