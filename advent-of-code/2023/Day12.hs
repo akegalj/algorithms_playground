d46f45912bd255b2b9a129a3200907af91422f8c
Ante Kegalj <akegalj@gmail.com>
2024-04-09 17:13:16 +0200

    aoc2023, part2, recursive try1

--- advent-of-code/2023/Day12.hs
+++ advent-of-code/2023/Day12.hs
@@ -7,14 +7,14 @@ import qualified Data.Map as M
 import Data.Function (on)
 import Data.Bits (setBit, Bits, (.|.), shiftL, (.&.), testBit)
 import Data.Maybe (fromMaybe)
+import Control.Applicative ((<|>))
 import Debug.Trace
 
 data Spring = Operational | Damaged | Unknown deriving (Eq, Show)
 newtype GroupSize = GroupSize Int
   deriving stock (Eq, Ord, Show)
-  deriving newtype Read
+  deriving newtype (Read, Num)
 type ConRec = ([Spring], [GroupSize])
-type MustUse = Bool
 
 parseInput :: String -> [ConRec]
 parseInput = map parseLine . lines
@@ -24,47 +24,25 @@ parseInput = map parseLine . lines
     parseSpring '#' = Damaged
     parseSpring '?' = Unknown
 
-indexGroups :: ConRec -> M.Map GroupSize [(MustUse, Int)]
-indexGroups (xs, gs) = M.fromList . map (id &&& index) . nub $ sort gs
-  where
-    sm = springMask xs :: Integer
-    lsm = length xs
-    toDamaged = map (\x -> if x == Unknown then Damaged else x) xs
-    contGroup = indexCont toDamaged
-    touchesDamaged (s,i) = testBit sm (lsm-i-s-1) || testBit sm (lsm-i)
-    constrainDamaged :: [(Int, Int)]
-    constrainDamaged = indexCont xs
-    index (GroupSize g) = concatMap (\(l, i) -> [(elem ix . map snd $ filter ((==g) . fst) constrainDamaged, ix) | ix <- [i..i+l-g], not $ touchesDamaged (g,ix)]) contGroup
-
-    indexCont = map (length &&& fst . head) . filter (any $ (==Damaged) . snd) . groupBy ((==) `on` snd) . zip [0..]
-
-springMask :: (Bits a, Num a) => [Spring] -> a
-springMask = foldl' setBit 0 . map fst . filter ((==Damaged) . snd) . zip [0..] . reverse
-
-indexMask :: (Bits a, Num a) => Int -> [(GroupSize, Int)] -> a
-indexMask l = foldl' (.|.) 0 . map (\(GroupSize g,i) -> shiftL (2^g-1) $ l-i-g)
-
-testMask :: Int -> ConRec -> [Int] -> Bool
-testMask sm (sx, gs) ix = sm .&. indexMask (length sx) (zip gs ix) == sm
-
-simulate :: ConRec -> [[Int]]
-simulate cr@(xs, gs) = go 0 $ zip gs groupIxs
-  where
-    ig = indexGroups cr
-    groupIxs = map (ig M.!) gs
-    go :: Int -> [(GroupSize, [(MustUse, Int)])] -> [[Int]]
-    go _ [] = [[]]
-    go !start ((GroupSize l, is):gs) = concatMap (\(_,i) -> map (i:) $ go (i+l+1) gs) $ dropAfter fst $ dropWhile ((start>) . snd) is
-    dropAfter _ [] = []
-    dropAfter f (x:xs)
-      | f x = x:[]
-      | otherwise = x : dropAfter f xs
+count :: GroupSize -> [Spring] -> [GroupSize] -> Maybe Int
+count 0   [] [] = Just 1
+count 0   [] [_] = Just 1
+count 0   (Damaged:_) _ = Nothing
+count 0   (Operational:ss) [_] = if Damaged `elem` ss then trace "woot" Nothing else Just 1
+count 0   (Operational:ss) (_:g:gs) = count g ss gs
+count cur (Operational:ss) ags@(g:_) = count g ss ags
+count cur (Damaged:ss) ags = count (cur-1) ss ags
+count cur (Unknown:ss) ags =
+  let c1 = count cur (Operational:ss) ags
+      c2 = count cur (Damaged:ss) ags
+  in ((+) <$> c1 <*> c2) <|> c1 <|> c2
+count a b _ = error $ show b
 
 part1 :: [ConRec] -> Int
-part1 = sum . map (\cr@(s,_) -> let sm = springMask s in length . filter (testMask sm cr) $ simulate cr)
+part1 = const 0
 
 part2 :: [ConRec] -> Int
-part2 = part1 . map (\(a,b) -> trace (show a) $ expandInput b) . zip [0..]
+part2 = const 0 -- part1 . map expandInput
 
 expandInput (sx, gs) = (concat $ intersperse [Unknown] $ replicate 5 sx, concat $ replicate 5 gs)
 
