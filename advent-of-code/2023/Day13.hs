36c6ef3ecedadf601c70ac7e7c7a0995a5744d8f
Ante Kegalj <akegalj@gmail.com>
2024-04-11 10:36:06 +0200

    aoc2023 day13 part1, refactor

--- advent-of-code/2023/Day13.hs
+++ advent-of-code/2023/Day13.hs
@@ -2,10 +2,12 @@ import Control.Arrow ((&&&))
 import qualified Data.Map as M
 import Data.List.Split (splitOn)
 import Data.List (transpose, tails, inits)
+import Control.Applicative ((<|>))
+import Data.Maybe (catMaybes)
 
 data Field = Dot | Hash deriving (Eq, Show)
-type Coord = (Int, Int)
 type Pattern = [[Field]]
+data Reflection = Horizontal Int | Vertical Int deriving (Eq, Show)
 
 parseInput :: String -> [Pattern]
 parseInput = map parsePattern . splitOn "\n\n"
@@ -14,17 +16,26 @@ parseInput = map parsePattern . splitOn "\n\n"
     parseField '.' = Dot
     parseField '#' = Hash
 
-horizontal :: Pattern -> [Int]
-horizontal xs = index . init . tail . zipWith isMirror (inits xs) $ tails xs
+horizontal :: Pattern -> Maybe Reflection
+horizontal p = fmap Horizontal . safeHead . index . init . tail . zipWith isMirror (inits p) $ tails p
   where
     index = map fst . filter snd . zip [1..]
     isMirror a = and . zipWith (==) (reverse a)
+    safeHead [] = Nothing
+    safeHead (x:_) = Just x
 
-vertical :: Pattern -> [Int]
-vertical = horizontal . transpose
+vertical :: Pattern -> Maybe Reflection
+vertical = fmap (\(Horizontal x) -> Vertical x) . horizontal . transpose
+
+reflection :: Pattern -> Maybe Reflection
+reflection p = horizontal p <|> vertical p
+
+score :: Reflection -> Int
+score (Horizontal x) = x * 100
+score (Vertical x) = x
 
 part1 :: [Pattern] -> Int
-part1 = sum . map (\p -> sum (horizontal p) * 100 + sum (vertical p))
+part1 = sum . map score . catMaybes . map reflection
 
 part2 :: [Pattern] -> Int
 part2 = const 0
