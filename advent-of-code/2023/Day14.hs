00fef95688b3fd5bf17e45e10d71760dcc1add18
Ante Kegalj <akegalj@gmail.com>
2024-04-12 13:07:20 +0200

    aoc2023, day14, part2 dynamic p

--- advent-of-code/2023/Day14.hs
+++ advent-of-code/2023/Day14.hs
@@ -1,9 +1,26 @@
+{-# LANGUAGE TypeFamilies #-}
 import Control.Arrow ((&&&))
-import Data.List (transpose, foldl')
+import Data.List (transpose, foldl', groupBy, sort, iterate')
 import Debug.Trace
+import Prelude hiding (cycle)
+import Data.MemoTrie
+import GHC.Generics (Generic)
 
-data Tile = Round | Cube | Space deriving (Eq, Show)
+data Tile = Round | Cube | Space deriving (Eq, Ord, Show, Generic)
 type Dish = [[Tile]]
+
+showDish :: Dish -> String
+showDish = unlines . map (map showTile)
+  where
+    showTile Round = 'O'
+    showTile Cube = '#'
+    showTile Space = '.'
+
+instance HasTrie Tile where
+  newtype (Tile :->: b) = TileTrie { unTileTrie :: Reg Tile :->: b }
+  trie = trieGeneric TileTrie
+  untrie = untrieGeneric unTileTrie
+  enumerate = enumerateGeneric unTileTrie
 
 parseInput :: String -> Dish
 parseInput = map (map parseRock) . lines
@@ -12,15 +29,26 @@ parseInput = map (map parseRock) . lines
     parseRock '#' = Cube
     parseRock '.' = Space
 
-part1 :: Dish -> Int
-part1 = sum . map (snd . foldl' count (0,0) . zip [0..] . reverse . (Cube:)) . transpose
+loadLeft :: Dish -> Int
+loadLeft = sum . map (snd . foldl' count (0,0) . zip [0..] . reverse . (Cube:))
   where
     count (c,s) (i,t)
       | t == Round = (c+1,s)
       | t == Cube = (0, s + i*c - c*(c-1) `div` 2)
       | otherwise = (c,s)
 
+cycle :: Dish -> Dish
+cycle = memo $ pullRight . pullDown . pullLeft . pullUp
+  where
+    pullLeft = map $ concatMap sort . groupBy (\a b -> a /= Cube && b /= Cube)
+    pullUp = transpose . pullLeft . transpose
+    pullRight = map reverse . pullLeft . map reverse
+    pullDown = reverse . pullUp . reverse -- transpose . pullRight . transpose
+
+part1 :: Dish -> Int
+part1 = loadLeft . transpose
+
 part2 :: Dish -> Int
-part2 = const 0
+part2 = sum . map part1 . take (10^9) . iterate' cycle
 
 main = interact $ show . (part1 &&& part2) . parseInput
