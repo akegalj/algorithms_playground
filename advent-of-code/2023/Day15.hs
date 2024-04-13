1de027d6a57a0b23df74d67acbfc078eec72525e
Ante Kegalj <akegalj@gmail.com>
2024-04-13 20:07:28 +0200

    aoc2023, day15, part2 ReadP

--- advent-of-code/2023/Day15.hs
+++ advent-of-code/2023/Day15.hs
@@ -1,20 +1,63 @@
 import Control.Arrow ((&&&))
 import Data.List.Split (splitOn)
-import Data.Char (ord)
+import Data.Char (ord, isAlpha, isNumber)
 import Data.List (foldl')
+import qualified Data.Map as M
+import Text.ParserCombinators.ReadP
+import Control.Applicative ((<|>))
+import Debug.Trace
 
-type Input = [String]
+newtype Hash = Hash { getHash :: Int } deriving (Eq, Ord, Show)
+newtype FocalLength = FocalLength Int deriving (Eq, Show)
+newtype Label = Label String deriving (Eq, Show)
+data Lens = Lens Label FocalLength deriving (Show, Eq)
+data Operation = Remove Label | Insert Lens deriving Show
 
-parseInput :: String -> Input
-parseInput = splitOn ","
+type Box = [Lens]
+type Book = M.Map Hash Box
 
-hash :: String -> Int
-hash = foldl' (\h c -> (h + ord c)*17 `rem` 256) 0
+labelP = Label <$> many (satisfy isAlpha) -- should be isAscii
+focalLengthP = FocalLength <$> read <$> many (satisfy isNumber)
+lensP = Lens <$> labelP <*> (char '=' *> focalLengthP)
 
-part1 :: Input -> Int
-part1 = sum . map hash
+operationP :: ReadP Operation
+operationP = removeP <|> insertP
+  where
+    removeP = Remove <$> labelP <* char '-'
+    insertP = Insert <$> lensP
 
-part2 :: Input -> Int
-part2 = undefined
+parseInput :: String -> [Operation]
+parseInput = fst . last . readP_to_S (sepBy operationP $ char ',')
+
+hash :: Operation -> Hash
+hash (Remove (Label l)) = runHash $ l <> "-"
+hash (Insert (Lens (Label l) (FocalLength fl))) = runHash $ l <> "=" <> show fl
+
+labelHash :: Operation -> Hash
+labelHash (Remove (Label l)) = runHash l
+labelHash (Insert (Lens (Label l) _)) = runHash l
+
+runHash :: String -> Hash
+runHash = Hash . foldl' (\h c -> (h + ord c)*17 `rem` 256) 0
+
+runOperation :: Book -> Operation -> Book
+runOperation m op@(Remove l) = M.update (Just . filter (\(Lens oldl _) -> l /= oldl)) (labelHash op) m
+runOperation m op@(Insert l) = M.insertWith ins (labelHash op) [l] m
+  where
+    ins newl [] = newl
+    ins [newl@(Lens l1 _)] (oldl@(Lens l2 _):xs)
+      | l1 == l2 = newl:xs
+      | otherwise = oldl:ins [newl] xs
+
+focusingPower :: Book -> [Int]
+focusingPower = concatMap (\(Hash k,xs) -> map (\(i,l) -> succ k * i * getFL l) $ zip [1..] xs ) . M.assocs
+  where
+    getFL (Lens _ (FocalLength l)) = l
+
+part1 :: [Operation] -> Int
+part1 = sum . map (getHash . hash)
+
+part2 :: [Operation] -> Int
+part2 = sum . focusingPower . foldl' runOperation mempty
 
 main = interact $ show . (part1 &&& part2) . parseInput
