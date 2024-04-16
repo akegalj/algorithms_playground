a861230f71f07043301076818c58dfe15cdf30d8
Ante Kegalj <akegalj@gmail.com>
2024-04-16 13:25:54 +0200

    aoc2023, day17, part1 bsp

--- advent-of-code/2023/Day17.hs
+++ advent-of-code/2023/Day17.hs
@@ -1,19 +1,17 @@
 import Control.Arrow ((&&&))
 import qualified Data.Map as M
-import Data.Maybe (fromJust, catMaybes)
 import qualified Data.Set as S
 import Data.Bifunctor (first, second)
-import Control.Monad.State
-import Debug.Trace
-import Data.List (find)
-import Control.Monad (join)
+import Data.List (sortOn)
 
 data Direction = N | E | S | W deriving (Show, Eq, Enum, Ord)
 type Coord = (Int, Int)
-type Map = M.Map Coord Int
-type ResMap = M.Map (Int, (Coord,Direction)) (Maybe Int)
+type Steps = Int
+type Puzzle = M.Map Coord Int
+type CDS = ((Coord,Direction),Steps)
+type ICDS = (Int,(Coord,Direction),Steps)
 
-parseInput :: String -> Map
+parseInput :: String -> Puzzle
 parseInput = M.fromList . concatMap (\(y,l) -> map (\(x,c) ->((x,y), read [c])) l) . map (fmap $ zip [0..]) . zip [0..] . lines
 
 rotateLeft, rotateRight :: Direction -> Direction
@@ -26,57 +24,30 @@ move S = (,S) . second succ . fst
 move E = (,E) . first succ . fst
 move W = (,W) . first pred . fst
 
-showPath :: Map -> M.Map Coord Direction -> String
-showPath m visited = unlines $ map (\y -> [ if (x,y) `M.member` visited then '#' else '.' | x <- [0..maxX]]) [0..maxY]
+bsp :: Puzzle -> S.Set CDS -> (ICDS, S.Set ICDS) -> Int
+bsp pm seen ((res,cd@(c,d),s),xs)
+  | c == target = res
+  | otherwise = bsp pm newSeen . S.deleteFindMin $ toVisit <> xs
   where
-    ((maxX, maxY),_) = M.findMax m
+    (target,_) = M.findMax pm
+    fst3 (x,_,_) = x
+    snd3 (_,x,_) = x
+    thr3 (_,_,x) = x
+    fmove = flip move
+    addRes (res,(c,d),s) = (res + pm M.! c,(c,d),s)
+    validSteps = filter ((<4) . thr3)
+    validCoord = filter (\(_,cd@(c,_),s) -> (cd,s) `S.notMember` seen && c `M.member` pm)
+    toVisit = S.fromList . map addRes . validCoord . validSteps $
+      [ (res,(fmove cd d),succ s)
+      , (res,(fmove cd $ rotateLeft d),1)
+      , (res,(fmove cd $ rotateRight d),1)
+      ]
+    newSeen = seen <> S.map (snd3 &&& thr3) toVisit
 
-constructPath :: ResMap -> Int -> Coord -> [(Coord,Direction)]
-constructPath rm i c = (c,direction) : constructPath rm iNext undefined -- (fst $ move direction (c, direction))
-  where
-    results :: [Maybe ((Int, (Coord, Direction)), Maybe Int)]
-    results = map (\ix -> (ix,) <$> M.lookup ix rm) $ map (\d -> (i, (c,d))) [N .. W]
-    smallRes :: Maybe Int
-    smallRes = minJust $ map (join . fmap snd) results
-    Just ((_,(_,direction)),_) = fromJust $ find ((==smallRes) . join . fmap snd) results
-    iNext = undefined -- succ i
+part1 :: Puzzle -> Int
+part1 pm = bsp pm mempty ((0,((0,0),S),0),mempty)
 
-findPath :: Map -> State ResMap (Maybe Int)
-findPath m = go' m 0 ((0,0),E)
-  where
-    (final, finalHeat) = M.findMax m
-    (_, firstHeat) = M.findMin m
-    go' :: Map -> Int -> (Coord,Direction) -> State ResMap (Maybe Int)
-    go' m a b = do
-      resm <- get
-      case M.lookup (a,b) resm of
-        Just res -> pure res
-        Nothing -> do
-          res <- go m a b
-          modify' $ M.insert (a,b) res
-          pure res
-    go _ 3 _ = pure $ Nothing
-    go m steps x@(coord,dir)
-      | M.lookup coord m == Nothing = pure Nothing
-      | coord == final = pure . Just $ finalHeat
-      | otherwise = trace (show $ (steps,x)) $ do
-          let m' = M.delete coord m
-          dirs <- sequence $ map ($ dir)
-                [ go' m' (succ steps) . flip move x
-                , go' m' 0 . flip move x . rotateLeft
-                , go' m' 0 . flip move x . rotateRight
-                ]
-          pure $ (+) <$> M.lookup coord m <*> minJust dirs
-
-minJust :: (Ord a) => [Maybe a] -> Maybe a
-minJust xs
-  | all (==Nothing) xs = Nothing
-  | otherwise = Just . minimum $ catMaybes xs
-
-part1 :: Map -> Int
-part1 = fromJust . flip evalState mempty . findPath
-
-part2 :: Map -> Int
+part2 :: Puzzle -> Int
 part2 = const 0
 
 main = interact $ show . (part1 &&& part2 ) . parseInput
