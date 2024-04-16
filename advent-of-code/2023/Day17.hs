2a12e043df220460d9d91f77ce8bc17290061a54
Ante Kegalj <akegalj@gmail.com>
2024-04-16 19:38:32 +0200

    aoc2023, day17, part2 bfs

--- advent-of-code/2023/Day17.hs
+++ advent-of-code/2023/Day17.hs
@@ -2,7 +2,6 @@ import Control.Arrow ((&&&))
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Bifunctor (first, second)
-import Data.List (sortOn)
 
 data Direction = N | E | S | W deriving (Show, Eq, Enum, Ord)
 type Coord = (Int, Int)
@@ -23,31 +22,42 @@ move N = (,N) . second pred . fst
 move S = (,S) . second succ . fst
 move E = (,E) . first succ . fst
 move W = (,W) . first pred . fst
+fmove = flip move
 
-bsp :: Puzzle -> S.Set CDS -> (ICDS, S.Set ICDS) -> Int
-bsp pm seen ((res,cd@(c,d),s),xs)
-  | c == target = res
-  | otherwise = bsp pm newSeen . S.deleteFindMin $ toVisit <> xs
+bfs :: (Steps -> Bool) -> (Steps -> Bool) -> (ICDS -> [ICDS]) -> Puzzle -> S.Set CDS -> (ICDS, S.Set ICDS) -> Int
+bfs validFinish validStep nextSteps pm seen (icds@(res,cd@(c,d),s),xs)
+  | c == target && validFinish s = res
+  | otherwise = bfs validFinish validStep nextSteps pm newSeen . S.deleteFindMin $ toVisit <> xs
   where
     (target,_) = M.findMax pm
     fst3 (x,_,_) = x
     snd3 (_,x,_) = x
     thr3 (_,_,x) = x
-    fmove = flip move
     addRes (res,(c,d),s) = (res + pm M.! c,(c,d),s)
-    validSteps = filter ((<4) . thr3)
+    validSteps = filter (validStep . thr3)
     validCoord = filter (\(_,cd@(c,_),s) -> (cd,s) `S.notMember` seen && c `M.member` pm)
-    toVisit = S.fromList . map addRes . validCoord . validSteps $
+    toVisit = S.fromList . map addRes . validCoord . validSteps $ nextSteps icds
+    newSeen = seen <> S.map (snd3 &&& thr3) toVisit
+
+part1 :: Puzzle -> Int
+part1 pm = bfs (const True) (<4) nextSteps pm mempty ((0,((0,0),S),0),mempty)
+  where
+    nextSteps (res,cd@(c,d),s) =
       [ (res,(fmove cd d),succ s)
       , (res,(fmove cd $ rotateLeft d),1)
       , (res,(fmove cd $ rotateRight d),1)
       ]
-    newSeen = seen <> S.map (snd3 &&& thr3) toVisit
-
-part1 :: Puzzle -> Int
-part1 pm = bsp pm mempty ((0,((0,0),S),0),mempty)
 
 part2 :: Puzzle -> Int
-part2 = const 0
+part2 pm = min (bfs' S) (bfs' E)
+  where
+    bfs' d = bfs (>3) (<11) nextSteps pm mempty ((0,((0,0),d),0),mempty)
+    nextSteps (res,cd@(c,d),s)
+      | s < 4 = [ (res,(fmove cd d),succ s) ]
+      | otherwise =
+          [ (res,(fmove cd d),succ s)
+          , (res,(fmove cd $ rotateLeft d),1)
+          , (res,(fmove cd $ rotateRight d),1)
+          ]
 
 main = interact $ show . (part1 &&& part2 ) . parseInput
